db257e8e1b25cffde8974533169f7b44d38c85a5
kresimirprlic <44910771+kresimirprlic@users.noreply.github.com>
2021-02-02 09:40:07 +0100

    Add files via upload

---
+++ precache-manifest.42f6ea54fa072ef514c6f69e820f6403.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045653157d9202e372095e232179d159",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c855140e5c7186ae103",
+    "url": "/static/css/main.c318d649.chunk.css"
+  },
+  {
+    "revision": "99dac22f07ca68e9b86d",
+    "url": "/static/js/2.3c25f1f0.chunk.js"
+  },
+  {
+    "revision": "c80b09f21c3dd2137be347c65d78f97d",
+    "url": "/static/js/2.3c25f1f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c855140e5c7186ae103",
+    "url": "/static/js/main.160f54ad.chunk.js"
+  },
+  {
+    "revision": "222c80fab58252d9729c",
+    "url": "/static/js/runtime-main.6413914c.js"
+  },
+  {
+    "revision": "27409af01cdfeb89f0be03e587fef6b8",
+    "url": "/static/media/bg-1.27409af0.jpg"
+  },
+  {
+    "revision": "da4889d38585111bef2560674337ca18",
+    "url": "/static/media/pexels-pineaple.da4889d3.jpeg"
+  }
+]);
